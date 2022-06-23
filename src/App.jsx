1d2e07eab3b78d5ef570c944ac0036e41fd11b95
danut07 <danny201312@gmail.com>
2022-06-24 00:06:05 +0300

    new-api-key

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import "./App.css";
 
 const cookies = new Cookies();
 
-const apiKey = "sxuamkdw36vr";
+const apiKey = "twx8jtzkvzxa";
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
